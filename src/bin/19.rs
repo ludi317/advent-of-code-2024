c01a70ae8c895b8a6b5719c58b4cb30dac741578
Ludi Rehak <ludi317@gmail.com>
2024-12-18 21:50:52 -0800

    Day 19. I've seen this problem before. Count number of ways a string can be composed of a set of substrings.

---
+++ src/bin/19.rs
@@ -0,0 +1,63 @@
+advent_of_code::solution!(19);
+
+pub fn part_one(input: &str) -> Option<usize> {
+    let mut lines = input.lines();
+    let patterns: Vec<&str> = lines.next().unwrap().split(", ").collect();
+    lines.next();
+    let sum = lines.filter(|line| can_make(&patterns, line)).count();
+    Some(sum)
+}
+
+fn can_make(patterns: &Vec<&str>, design: &str) -> bool {
+    let mut dp = vec![false; design.len()+1];
+    dp[0] = true;
+    for i in 0..design.len() {
+        for &p in patterns {
+            if dp[i] && i + p.len() <= design.len() &&
+                design[i..i + p.len()] == *p {
+                dp[i+ p.len()] = true
+            }
+        }
+    }
+    dp[design.len()]
+}
+
+fn num_ways(patterns: &Vec<&str>, design: &str) -> usize {
+    let mut dp = vec![0; design.len()+1];
+    dp[0] = 1;
+    for i in 0..design.len() {
+        for &p in patterns {
+            if dp[i] > 0 && i + p.len() <= design.len() &&
+                design[i..i + p.len()] == *p {
+                dp[i+ p.len()] += dp[i]
+            }
+        }
+    }
+    dp[design.len()]
+}
+
+
+pub fn part_two(input: &str) -> Option<usize> {
+    let mut lines = input.lines();
+    let patterns: Vec<&str> = lines.next().unwrap().split(", ").collect();
+    lines.next();
+    let sum = lines.map(|line| num_ways(&patterns, line)).sum();
+    Some(sum)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(6));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(16));
+    }
+}
